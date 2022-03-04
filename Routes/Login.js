6c63b0a184eda96ab6d918ebc24ec8672401279e
Ragav1196 <ragavinrap@gmail.com>
2022-03-04 17:57:29 +0530

    test

--- Routes/Login.js
+++ Routes/Login.js
@@ -1,44 +1,46 @@
 import express from "express";
-import { GetEmail, GenerateToken } from "../Utilities/FUNCTIONS/Users.js";
-import bcrypt from "bcrypt";
+// import { GetEmail, GenerateToken } from "../Utilities/FUNCTIONS/Users.js";
+// import bcrypt from "bcrypt";
 
 const router = express.Router();
 
 // LOGIN
 router.route("/login").get(async (req, res) => {
-  const dataProvided = req.body;
 
-  // GETTING USER DATA FROM DATABASE
-  const DataFrmDB = await GetEmail(dataProvided.email);
+  res.send({message: "HAI"})
+  // const dataProvided = req.body;
 
-  // TO THROW AN ERROR IF USER IS NOT PRESENT IN THE DATABASE
-  if (!DataFrmDB) {
-    res.status(400).send({ message: "Invalid credentials", Access: false });
-    return;
-  }
+  // // GETTING USER DATA FROM DATABASE
+  // const DataFrmDB = await GetEmail(dataProvided.email);
 
-  // VERIFYING IF BOTH PASSWORD FROM DATABASE AND ONE GIVEN BY USER IS SAME OR NOT
-  const storedPassword = DataFrmDB.password;
-  const isPasswordMatch = await bcrypt.compare(
-    dataProvided.password,
-    storedPassword
-  );
+  // // TO THROW AN ERROR IF USER IS NOT PRESENT IN THE DATABASE
+  // if (!DataFrmDB) {
+  //   res.status(400).send({ message: "Invalid credentials", Access: false });
+  //   return;
+  // }
 
-  // TO SEND SUCCESS REPOSNSE TO THE FRONT END IF PASSWORD MATCHES
-  if (isPasswordMatch) {
-    // GENERATING TOKEN
-    const token = GenerateToken(DataFrmDB);
+  // // VERIFYING IF BOTH PASSWORD FROM DATABASE AND ONE GIVEN BY USER IS SAME OR NOT
+  // const storedPassword = DataFrmDB.password;
+  // const isPasswordMatch = await bcrypt.compare(
+  //   dataProvided.password,
+  //   storedPassword
+  // );
 
-    res.send({
-      message: "Successfull login",
-      token: token,
-      Access: true,
-    });
-  }
-  // TO SEND ERROR MESSAGE IF THE PASSWORS IS NOT A MATCH
-  else {
-    res.status(401).send({ message: "Invalid credentials", Access: false });
-  }
+  // // TO SEND SUCCESS REPOSNSE TO THE FRONT END IF PASSWORD MATCHES
+  // if (isPasswordMatch) {
+  //   // GENERATING TOKEN
+  //   const token = GenerateToken(DataFrmDB);
+
+  //   res.send({
+  //     message: "Successfull login",
+  //     token: token,
+  //     Access: true,
+  //   });
+  // }
+  // // TO SEND ERROR MESSAGE IF THE PASSWORS IS NOT A MATCH
+  // else {
+  //   res.status(401).send({ message: "Invalid credentials", Access: false });
+  // }
 });
 
 export const LoginRouter = router;
