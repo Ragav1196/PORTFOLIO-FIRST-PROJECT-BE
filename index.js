b89929d75b8f8290251add64a49683e3ef128859
Ragav1196 <ragavinrap@gmail.com>
2022-03-04 17:09:05 +0530

    test

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
-import { LoginRouter } from "./routes/Login.js";
-import { SignUpRouter } from "./routes/SignUp.js";
-import { AddFriendRouter } from "./Routes/Friends Channel/AddFriend.js";
-import { FriendsExpensesRouter } from "./Routes/Friends Channel/FriendsExpenses.js";
-import { AddGroupRouter } from "./Routes/Group Channel/AddGroups.js";
-import { GroupExpensesRouter } from "./Routes/Group Channel/GroupExpenses.js";
-import { GetFriendsRouter } from "./Routes/Friends Channel/GetFriends.js";
-import { GetGroupsRouter } from "./Routes/Group Channel/GetGroups.js";
-import { GetFriendsExpensesRouter } from "./Routes/Friends Channel/GetFriendsExpenses.js";
-import { GetGroupExpensesRouter } from "./Routes/Group Channel/GetGroupExpenses.js";
+// import { LoginRouter } from "./routes/Login.js";
+// import { SignUpRouter } from "./routes/SignUp.js";
+// import { AddFriendRouter } from "./Routes/Friends Channel/AddFriend.js";
+// import { FriendsExpensesRouter } from "./Routes/Friends Channel/FriendsExpenses.js";
+// import { AddGroupRouter } from "./Routes/Group Channel/AddGroups.js";
+// import { GroupExpensesRouter } from "./Routes/Group Channel/GroupExpenses.js";
+// import { GetFriendsRouter } from "./Routes/Friends Channel/GetFriends.js";
+// import { GetGroupsRouter } from "./Routes/Group Channel/GetGroups.js";
+// import { GetFriendsExpensesRouter } from "./Routes/Friends Channel/GetFriendsExpenses.js";
+// import { GetGroupExpensesRouter } from "./Routes/Group Channel/GetGroupExpenses.js";
 
 dotenv.config();
 
@@ -22,35 +22,35 @@ const MONGO_URL = process.env.MONGO_URL;
 app.use(cors());
 app.use(express.json());
 
-// SIGN UP
-app.use("/sign-up", SignUpRouter);
+// // SIGN UP
+// app.use("/sign-up", SignUpRouter);
 
-// LOGIN
-app.use("/login", LoginRouter);
+// // LOGIN
+// app.use("/login", LoginRouter);
 
-// ADD FRIEND
-app.use("/add-friends", AddFriendRouter);
+// // ADD FRIEND
+// app.use("/add-friends", AddFriendRouter);
 
-// GET FRIENDS
-app.use("/get-friends", GetFriendsRouter);
+// // GET FRIENDS
+// app.use("/get-friends", GetFriendsRouter);
 
-// ADD EXPENSE BETWEEN FRIENDS
-app.use("/add-friends-expenses/", FriendsExpensesRouter);
+// // ADD EXPENSE BETWEEN FRIENDS
+// app.use("/add-friends-expenses/", FriendsExpensesRouter);
 
-// GET FRIENDS EXPENSES
-app.use("/get-friends-expenses", GetFriendsExpensesRouter);
+// // GET FRIENDS EXPENSES
+// app.use("/get-friends-expenses", GetFriendsExpensesRouter);
 
-// ADD GROUPS
-app.use("/add-groups", AddGroupRouter);
+// // ADD GROUPS
+// app.use("/add-groups", AddGroupRouter);
 
-// ADD GROUPS EXPENSE
-app.use("/add-groups-expenses", GroupExpensesRouter);
+// // ADD GROUPS EXPENSE
+// app.use("/add-groups-expenses", GroupExpensesRouter);
 
-// GET GROUPS
-app.use("/get-groups", GetGroupsRouter);
+// // GET GROUPS
+// app.use("/get-groups", GetGroupsRouter);
 
-// GET GROUPS EXPENSES
-app.use("/get-groups-expenses", GetGroupExpensesRouter);
+// // GET GROUPS EXPENSES
+// app.use("/get-groups-expenses", GetGroupExpensesRouter);
 
 // CREATING MONGO CONNECTION
 async function CreateConnection() {
